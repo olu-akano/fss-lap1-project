1cff35de481c0a9e9bb661ebb262e5c92ccaeb78
Oluwaseun Akano <90205470+olu-akano@users.noreply.github.com>
2021-10-05 17:49:35 +0100

    add js to emoji functionality

--- client/emoji.js
+++ client/emoji.js
@@ -1,8 +1,20 @@
 let laughButton = document.getElementById('laugh')
+let sadButton = document.getElementById('sad')
+let coolButton = document.getElementById('cool')
 const laughEmoji = e.target.laugh.value
 
 count = 0;
 laughButton.onclick = function() {
     count +=1;
-    laughButton.textContent = laughEmoji + count;
+    laughButton.textContent = '&#128514' + count;
 };
+
+sadButton.onclick = function () {
+    count +=1;
+    sadButton.textContent = '&#128532' + count;
+}
+
+sadButton.onclick = function () {
+    count +=1;
+    sadButton.textContent = '&#128526' + count;
+}
