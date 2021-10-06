ab9a534e6eb46b5e9ef6b930fffbbc544bf92974
Cameron <cameronbromley8@gmail.com>
2021-10-06 23:36:00 +0100

    progress made with script.js!!!!

--- client/assets/script.js
+++ client/assets/script.js
@@ -16,48 +16,50 @@ popUp.addEventListener('mouseleave', (e) => {
 
 gifBtn.addEventListener("click", gifselection);
 
-function showGif (content){
-    popUp.style.display = "grid"
-    let gifUrl = content.data;
-    let i = 1;
-    for(const gif of gifUrl){
-        let fig = document.createElement('div');
-        fig.classList.add("gifchoice");
-        // let img = document.createElement('img');
-        // let fc = document.createElement('figcaption');
-        // img.src = content.data[i].images.downsized_medium.url;
-        // img.alt = content.data[i].title;
-        // img.id = 'gif'
-        // fc.textContent = content.data[i].title;
-        // fig.append(img);
-        // fig.append(fc);
-        // let gif = img.src
-        fig.innerHTML = `<figure id="gif${i}" class='gif ' ><img src='${content.data[i].images.downsized_medium.url}' 
-        alt='${content.data[i].title}' style= "width:100px"> <figcaption> ${content.data[i].title} </figcaption> </figure>`;
-        popUp.append(fig);
-        // console.log(gif)
-        // let output = document.querySelector('.output')
-        // output.insertAdjacentElement('afterbegin', fig);
-        // document.querySelector('#gifInput').value ='';
-        i++
-        clicked(gif)
-    }
-}
+
 
 async function gifselection (e) {
     e.preventDefault();
-    let str = document.getElementById("gifInput").value;
+    let str = document.getElementById("gifInput").value.trim();
     str.value = '';
     let apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=9&q=`
-    // apiURL = apiURL.concat(str);
+    apiURL = apiURL.concat(str);
     try {
         let data = await fetch(apiURL);
         let dataJson = await data.json();
+        console.log(dataJson);
         showGif(dataJson);
     } catch (err) {console.warn(err)}
 }
 
-
+function showGif (content){
+    popUp.style.display = "grid"
+    let gifUrl = content.data;
+    console.log(gifUrl);
+    for(let i=0; l=gifUrl.length; i++){
+        let fig = document.createElement('div');
+        fig.classList.add("gif");
+        let img = document.createElement('img');
+        let fc = document.createElement('figcaption');
+        img.src = content.data[i].images.downsized_medium.url;
+        img.alt = content.data[i].title;
+        img.id = 'gif'
+        fc.textContent = content.data[i].title;
+        fig.append(img);
+        fig.append(fc);
+        let gif = img.src
+        // fig.innerHTML = `<figure id="gif${i}" class='gif ' ><img src='${content.data[i].images.downsized_medium.url}' 
+        // alt='${content.data[i].title}' style= "width:100px"> <figcaption> ${content.data[i].title} </figcaption> </figure>`;
+        // popUp.append(fig);
+        // console.log(gif)
+        popUp.append(fig)
+        let output = document.querySelector('.output')
+        output.insertAdjacentElement('afterbegin', fig);
+        document.querySelector('#gifInput').value ='';
+        
+        clicked(gif)
+    }
+}
 
 
 
@@ -85,7 +87,9 @@ async function getAll(){
         let cardbox = document.getElementById('card--container');
         for(let i = 0; i < jsonData.length; i++){
             let card = document.createElement('div');
-            card.innerHTML = `<div id="card${i}" class="card"><a class='entryContent' href="thread.html?${jsonData[i].id}">${jsonData[i].body}</a></div>`;
+            card.classList.add('card')
+            card.id = `card${i}`
+            card.innerHTML = `<a class='entryContent' href="thread.html?${jsonData[i].id}">${jsonData[i].body}</a>`;
             cardbox.append(card);
         }
     }catch(err){
