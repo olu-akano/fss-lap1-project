26359f67e2afc117ead3b1f1d46e54cf0fca3e2b
Zain1Uppal <83920992+Zain1Uppal@users.noreply.github.com>
2021-10-06 10:40:46 +0100

    update giphy.js

--- client/giphy.js
+++ client/giphy.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", sendApiRequest)
 function sendApiRequest() {
     document.getElementById('gifButton').addEventListener("click", e => {
         e.preventDefault();
-        let apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=1&q=`
+        let apiURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=3&q=`
         let str = document.getElementById("gifInput").value.trim();
         apiURL = apiURL.concat(str);
         console.log(apiURL);
@@ -15,23 +15,42 @@ function sendApiRequest() {
         .then(res => res.json())
         .then(content => {
             console.log(content.data);
-            console.log("META", content.meta);
-            let fig = document.createElement('figure');
-            let img = document.createElement('img');
-            let fc = document.createElement('figcaption');
-            img.src = content.data[0].images.downsized.url;
-            img.alt = content.data[0].title;
-            fc.textContent = content.data[0].title;
-            fig.appendChild(img);
-            fig.appendChild(fc);
-            let output = document.querySelector('.output')
-            output.insertAdjacentElement('afterbegin', fig);
-            document.querySelector('#gifInput').value ='';
+            let gifUrl = content.data;
+            let randomGif = Math.floor((Math.random() * 3));
+            // console.log(content.data[0].images.downsized.url)
+            for(let i = 0; i < gifUrl.length;i++){
+                let fig = document.createElement('figure');
+                let img = document.createElement('img');
+                let fc = document.createElement('figcaption');
+                img.src = content.data[i].images.downsized.url;
+                img.alt = content.data[i].title;
+                img.id = 'gif'
+                fc.textContent = content.data[i].title;
+                fig.appendChild(img);
+                fig.appendChild(fc);
+                let gif = img.src
+                let output = document.querySelector('.output')
+                output.insertAdjacentElement('afterbegin', fig);
+                document.querySelector('#gifInput').value ='';
+                clicked(gif)
+            }
         })
         .catch(err =>{
             console.error(`Oh no.. ${err}`)
         
         });
     })
+
+}
+function clicked(url){
+    let chosenGif = document.getElementById('gif')
+    let clickedGif = document.getElementById('chosen')
+    chosenGif.addEventListener('click', (e) => {
+
+        clickedGif.innerHTML = `<p>this is the img u chose</p> <img src = "${url}"></img>`
+    })
+    return url
 }
 
+// clicked()
+console.log(gif)
